0c788fa0bed54ad41943529b2cfc074b662c3335
Kaustubha_Pundir <kaustubhapundir@gmail.com>
2024-01-04 12:43:31 +0530

    Selection_sort.java

---
+++ Selection_sort.java
@@ -0,0 +1,31 @@
+public class Selection_sort {
+    public static void main(String[] args) {
+        int[] arr={4,5,1,2,3,9,7};
+        for (int i=0;i<arr.length;i++){
+            int last=arr.length-i-1;
+
+            int max=max_inarr(arr,last);
+            swap(last,max,arr);
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]+"");
+        }
+    }
+    static void swap(int last,int max,int[] arr){
+        int temp=arr[last];
+        arr[last]=arr[max];
+        arr[max]=temp;
+    }
+    static int max_inarr(int[] arr,int last){
+        int max=0;
+        for(int j=0;j<=last;j++) {
+            if (arr[j] > arr[max]) {
+                max = j;
+            }
+        }
+        return max;
+    }
+}
+//best case --> O(n^2)
+//worst case --> O(n^2)
+//stable --> No
