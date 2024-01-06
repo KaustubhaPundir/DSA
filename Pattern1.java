88fe5d4822b08dc0ace627f0a71cdd3a1cdaba74
Kaustubha_Pundir <kaustubhapundir@gmail.com>
2024-01-06 20:44:38 +0530

    Pattern1.java

--- Pattern1.java
+++ Pattern1.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+public class Pattern1 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter the number of rows of the pattern: ");
+        int n= input.nextInt();
+        pattern(n);
+    }
+    static void pattern(int n){
+        for(int row=1;row<=n;row++){
+            for(int col=1;col<=row;col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
