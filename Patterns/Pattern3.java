5ed97b8da51ad12959e1d017f26488c9359e4048
Kaustubha_Pundir <kaustubhapundir@gmail.com>
2024-01-06 22:32:38 +0530

    Create Pattern3.java

---
+++ Patterns/Pattern3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Pattern3 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter any integer: ");
+        int n= input.nextInt();
+        pattern(n);
+    }
+    static void pattern(int n){
+        for(int row=1;row<=n;row++){
+            for(int sp=n-row;sp>0;sp--){
+                System.out.print(" ");
+            }
+            for(int col=1;col<=row;col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
