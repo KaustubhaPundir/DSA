523bda74737a0302a82d8be63ff72d25b0de2180
Kaustubha_Pundir <kaustubhapundir@gmail.com>
2024-02-03 22:23:52 +0530

    String Builder

---
+++ Strbuilder.java
@@ -0,0 +1,9 @@
+public class Strbuilder {
+    public static void main(String[] args) {
+        StringBuilder string=new StringBuilder();
+        string.append("abc");
+        System.out.println(string);
+        string.reverse();
+        System.out.println(string);
+    }
+}
